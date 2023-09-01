befc3054543b9e49159e8242edbe77091908ad71
Monse125 <monserrat.montero@ug.uchile.cl>
2023-09-01 16:23:06 -0400

    Base de animaciones del personaje y mejora de fisicas

--- Player.gd
+++ Player.gd
@@ -1,28 +1,21 @@
 extends CharacterBody2D
 
+var speed = 250
+var gravity = 300
+var jump_speed = 250
+var aceleration = 10000
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 750 
-
+@onready var anim = $AnimationPlayer
 
 func _physics_process(delta):
-	# Add the gravity.
+	var move_input = Input.get_axis("left","right")
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
+		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y = -jump_speed
+	
+	velocity.x = move_toward(velocity.x,speed * move_input, aceleration * delta)
+	
 	move_and_slide()
+		
