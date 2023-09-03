920b633dd78a150a6337c915888bcd9229fe5a89
Monse125 <monserrat.montero@ug.uchile.cl>
2023-09-03 03:53:53 -0300

    Implementacion de animacion para light y un pivote. Pruebas con Raycast2D

--- Player.gd
+++ Player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
 var speed = 250
-var gravity = 300
+var gravity = 400
 var jump_speed = 250
 var aceleration = 10000
 
-@onready var anim = $AnimationPlayer
+#REFERENCIAS
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
+@onready var pivote: Node2D = $Pivote
+
+func _ready() -> void:
+	animation_tree.active = true
+	
+	
+#MOVIMIENTO
 
 func _physics_process(delta):
+	#Movimiento del personaje
 	var move_input = Input.get_axis("left","right")
+	var sign_Move_input = sign(move_input)
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
@@ -18,4 +31,29 @@ func _physics_process(delta):
 	velocity.x = move_toward(velocity.x,speed * move_input, aceleration * delta)
 	
 	move_and_slide()
+	
+	"""
+	#Pruebas del Raycast
+	var space_state = get_world_2d().direct_space_state
+	var x_rayo1 = global_position.x + (200*sign_Move_input)
+	var y_rayo1 = global_position.y
+	var query = PhysicsRayQueryParameters2D.create(global_position,Vector2(x_rayo1,y_rayo1))
+	query.exclude = [self]
+	var result = space_state.intersect_ray(query)
+	if result:
+		print("Hit at point: ", result.position)
+	"""
+	
+	
+	
+	#ANIMACIONES	
+	if move_input != 0:
+		pivote.scale.x = sign_Move_input
 		
+	if Input.is_action_pressed("light"):
+		playback.travel("light")
+	else:
+		playback.travel("idle")
+		
+		
+
