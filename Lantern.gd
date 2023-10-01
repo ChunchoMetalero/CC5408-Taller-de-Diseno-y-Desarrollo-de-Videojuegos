86d414be6f3968a6a306238c88b9a39739858698
Monse125 <monserrat.montero@ug.uchile.cl>
2023-10-01 17:40:57 -0300

    Implementación de un rayo de la lantern

--- Lantern.gd
+++ Lantern.gd
@@ -1,24 +1,32 @@
 extends StaticBody2D
 
-
-
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
-@onready var line_2d: Line2D = $Line2D
-@onready var pivote: Node2D = $Pivote
+@onready var linea: Line2D = Line2D.new() 
 
 var angule: float = PI/3
+var origen: Vector2 = Vector2(0,0)
+var target_pos = origen + Vector2(1000,0)
+
 
 func _ready() -> void:
-	get_tree().get_
+	linea.add_point(origen)
+	linea.add_point(origen)
+	add_child(linea)  # Agregar la línea como un hijo del nodo StaticBody2D
+	linea.default_color = Color(1, 0, 0)  # Color rojo
+	linea.width = 2.0  # Ancho de línea
 	
-	line_2d.add_point(Vector2(0,0))
-	line_2d.add_point(Vector2(0,0))
-	ray_cast_2d.target_position = Vector2(cos(angule),sin(angule))
-
 
 func _physics_process(delta: float) -> void:
-	var scala = pivote.scale
 	if Input.is_action_pressed("light"):
-		line_2d.points[1] = (Vector2(cos(angule),sin(angule))) * scala
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(origen+position, target_pos+position)
+		var result = space_state.intersect_ray(query)
+		if result:
+			print("Hit at point: ", result.position)
+			linea.points[1] = result.position - position
+		else:
+			linea.points[1] = target_pos
 	else:
-		line_2d.points[1] = Vector2.ZERO
+		linea.points[1] = origen
+		
+			
+	
