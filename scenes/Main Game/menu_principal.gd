95e3ee6182baffa30d780b20439d107938580c5f
VictorPetersV <victor.peters@ug.uchile.cl>
2023-10-22 23:39:49 -0300

    Se añade funcionamiento al menu principal

---
+++ scenes/Main Game/menu_principal.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/Iniciar.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_iniciar_pressed():
+	get_tree().change_scene_to_file("res://scenes/Elementos/main.tscn")
+	
+
+
+func _on_salir_pressed():
+	get_tree().quit()
+
+
+func _on_niveles_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main Game/niveles.tscn")
