d1e1c38e09c0c2316dfcc3694299c579d4f5bfbf
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2023-12-12 21:32:43 -0300

    Win condition, loose condition, level manager updated

--- scenes/Main Game/menu_principal.gd
+++ scenes/Main Game/menu_principal.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_iniciar_pressed():
-	get_tree().change_scene_to_file("res://scenes/Elementos/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/Levels/Level0.tscn")
 	
 
 
